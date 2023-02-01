9c30071b49d286fb582632668aacf642f35940a0
unknowmode@gmail.com <unknowmode@gmail.com>
2023-02-01 13:36:20 +0300

    правки текста

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import telebot, datetime, requests
-from telebot import types
 bot = telebot.TeleBot("5800314423:AAFytkDuFON5M5eBENHaC0Iqg7DDQz1op0I")
-# отслеживание команды /start
+tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
 @bot.message_handler(commands=['start'])
 def start(message):
     hello = f'Привет, <b>{message.from_user.first_name}</b>.'\
@@ -9,16 +8,14 @@ def start(message):
     bot.send_message(message.chat.id, hello, parse_mode='html')
 @bot.message_handler(commands=['help'])
 def start(message):
-    # отправка привет пользователю + Имя
     mess = f' Для записи в столовую на <u>завтра</u> введите свою фамилию следующим сообщением.'\
-    f'\nЭту операцию необходимо проделывать каждый раз для записи.'
-    # отправка в тот же чат по id
-    answer = bot.send_message(message.chat.id, mess, parse_mode='html');
-    # ловим ответ пользователя
+    f'\nЭту операцию необходимо проделывать каждый раз для записи (/help->Фамилия).'
+    answer = bot.send_message(message.chat.id, mess, parse_mode='html')
     bot.register_next_step_handler(answer, review)
-tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
 def review(message):
-    save_mess = message.text + tomorrow.strftime('%d.%m.%Y')
-    print(save_mess)
+    save_mess ='&user='+ message.text +'&date='+ tomorrow.strftime('%d-%m-%Y')
+    url='http://sqlsrv/multi/hs/Canteen/UsersList/?id=123&pp=123'+save_mess
+    send_post = requests.get(url)
+    print(send_post)
 
 bot.polling(none_stop=True)
