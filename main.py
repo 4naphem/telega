0f027cda9caba5aafc75bd8ce3752710862f19af
unknowmode@gmail.com <unknowmode@gmail.com>
2023-02-02 13:09:41 +0300

    added time check

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import telebot, datetime, requests
-bot = telebot.TeleBot("5800314423:AAFytkDuFON5M5eBENHaC0Iqg7DDQz1op0I")
+bot = telebot.TeleBot("5800314423:AAGrbowq4JXK5koSlUNXXd2q9Joc8sF8mvk")
 tomorrow = datetime.datetime.today() + datetime.timedelta(days=1)
+now = datetime.datetime.now()
+today_9_10 = now.replace(hour=9, minute=10)
 @bot.message_handler(commands=['start'])
 def start(message):
     hello = f'Привет, <b>{message.from_user.first_name}</b>.'\
@@ -13,9 +15,14 @@ def start(message):
     answer = bot.send_message(message.chat.id, mess, parse_mode='html')
     bot.register_next_step_handler(answer, review)
 def review(message):
-    save_mess ='&user='+ message.text +'&date='+ tomorrow.strftime('%d-%m-%Y')
-    url='http://sqlsrv/multi/hs/Canteen/UsersList/?id=123&pp=123'+save_mess
-    send_post = requests.get(url)
+    save_mess_today = '&user='+ message.text +'&date='+ now.strftime('%d-%m-%Y')
+    save_mess_tomorrow = '&user='+ message.text +'&date='+ tomorrow.strftime('%d-%m-%Y')
+    if now < today_9_10:
+        url = 'http://sqlsrv/multi/hs/Canteen/UsersList/?id=123&pp=123' + save_mess_today
+        send_post = requests.get(url)
+    else:
+        url = 'http://sqlsrv/multi/hs/Canteen/UsersList/?id=123&pp=123' + save_mess_tomorrow
+        send_post = requests.get(url)
     print(send_post)
 
 bot.polling(none_stop=True)
