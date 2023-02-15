4dce81132443e91ac5c0f91b96c6109176c243e0
unknowmode@gmail.com <unknowmode@gmail.com>
2023-02-15 16:32:48 +0300

    Добавил try для исбежания поломки после неверного ввода ФИО

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import telebot, datetime, requests, logging, time
+import telebot
+import datetime
+import requests
+import logging
+import time
 
 logging.basicConfig(level='INFO', filename='telebot.log', format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger('MyLog')
@@ -20,25 +24,30 @@ def start(message):
 @bot.message_handler(commands=['help'])
 def start(message):
     mess = f' Для записи в столовую на <u>завтра</u> отправьте свои ФИО полностью как в паспорте.' \
-           f'\nЭту операцию необходимо проделывать каждый раз для записи: \n(/help->Фамилия).'
+           f'\nВводить данные нужно всегда после команды /help.'
     answer = bot.send_message(message.chat.id, mess, parse_mode='html')
     bot.register_next_step_handler(answer, review)
 
 
 def review(message):
-    answer = message.text
-    split = answer.split()
-    save_mess1 = '&FirstName=' + split[0] + '&SecondName=' + split[1] + "&LastName=" + split[2] + '&Date=' + now.strftime('%Y%m%d')
-    save_mess2 = 'Запись на сегодня закончена! Записаться можно до 9:10.\n' \
-                 'Для записи на завтра введите /tomorrow'
-    url = 'http://sqlsrv/multi/hs/Canteen/AddList?id=123' + save_mess1
-    ans = requests.get(url)
-    to_user = ans.json()
-    if now < today_9_10:
-        bot.send_message(message.chat.id, to_user['Ansfer'] + ' на ' + now.strftime('%d-%m-%Y'), parse_mode='html')
-        print(to_user['Ansfer'])
-    else:
-        bot.send_message(message.chat.id, save_mess2 , parse_mode='html')
+    try:
+        answer = message.text
+        split = answer.split()
+        save_mess1 = '&FirstName=' + split[0] + '&SecondName=' + split[1] + \
+                     "&LastName=" + split[2] + '&Date=' + now.strftime('%Y%m%d')
+        save_mess2 = 'Запись на сегодня закончена! Записаться можно до 9:10.\n' \
+                     'Для записи на завтра введите /tomorrow'
+        url = 'http://sqlsrv/multi/hs/Canteen/AddList?id=123' + save_mess1
+        ans = requests.get(url)
+        to_user = ans.json()
+        if now < today_9_10:
+            bot.send_message(message.chat.id, to_user['Ansfer'] + ' на ' + now.strftime('%d-%m-%Y'), parse_mode='html')
+            print(to_user['Ansfer'])
+        else:
+            bot.send_message(message.chat.id, save_mess2, parse_mode='html')
+            print(message.text + now.strftime(' %H:%M'))
+    except IndexError:
+        bot.send_message(message.chat.id, 'Введите данные как в паспорте после /help' , parse_mode='html')
 
 
 def telega_polling():
